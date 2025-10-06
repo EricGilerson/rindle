88133c2144347582d5e99068b759e54d3f321d74
Eric Gilerson <ericgilerson@gmail.com>
2025-10-06 15:03:17 -0400

    first

---
+++ include/rivulet/engine.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Eric Gilerson on 10/6/25.
+//
+
+#ifndef RIVULET_ENGINE_HPP
+#define RIVULET_ENGINE_HPP
+
+#endif //RIVULET_ENGINE_HPP
---
+++ include/rivulet/types.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Eric Gilerson on 10/6/25.
+//
+
+#ifndef RIVULET_TYPES_HPP
+#define RIVULET_TYPES_HPP
+
+#endif //RIVULET_TYPES_HPP
