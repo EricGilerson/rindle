d83983d07022040daaaf4370054626872cc819b9
Eric Gilerson <itssgilly@gmail.com>
2025-10-09 21:22:26 -0400

    add dataset_builder.cpp

---
+++ include/rivulet/dataset_builder.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Eric Gilerson on 10/9/25.
+//
+
+#ifndef RIVULET_DATASET_BUILDER_HPP
+#define RIVULET_DATASET_BUILDER_HPP
+
+
+class dataset_builder {
+};
+
+
+#endif //RIVULET_DATASET_BUILDER_HPP
---
+++ src/dataset_builder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eric Gilerson on 10/9/25.
+//
+
+#include "../include/rivulet/dataset_builder.hpp"
